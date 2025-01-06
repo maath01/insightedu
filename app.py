70367922b80361fd253dee551cff3d2fee576abf
maath01 <168108466+maath01@users.noreply.github.com>
2025-01-06 13:35:21 -0300

    Update app.py
    Deletei a função 'cadastro_alunos'

--- app.py
+++ app.py
@@ -10,11 +10,3 @@ banco.create_database()
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/cadastro_aluno')
-def cadastro_aluno():
-    esc = escola.Escola(6999, 'IFCE Campus Crato', 'Crato', 'CE')
-    al = aluno.Aluno(0, 'Matheus Soares do Nascimento', '12345678', '02/10/2004', '2024')
-    aluno.create(al, esc)
-
-    print('Aluno adicionado')
