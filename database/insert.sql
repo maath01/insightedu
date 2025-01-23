1ee7f63796d0a186e2eeb2aabdcd79140a471f66
Matheus Soares <matheus.soares10@aluno.ifce.edu.br>
2025-01-23 15:00:53 -0300

    modificado insert.sql para que usasse o sistema de ids

--- database/insert.sql
+++ database/insert.sql
@@ -1,30 +1,45 @@
-INSERT INTO escolas (id, nome, cidade, uf) VALUES (1, 'IFCE', 'Crato', 'CE');
+INSERT INTO escolas (id, nome, cidade, uf) VALUES
+(68921, 'IFCE', 'Crato', 'CE'),
+(60932, 'Maria Violeta Arraes', 'Crato', 'CE');
 
-INSERT INTO professores (id, nome, email, senha, uf, data_nascimento) VALUES (1, 'Ana Silva Souza', 'ana.silva@gmail.com', 'anass123', 'CE', '01-01-1995');
-INSERT INTO professores (id, nome, email, senha, uf, data_nascimento) VALUES (2, 'Roberto Gonçalves', 'roberto.goncalves@gmail.com', '12345678', 'CE', '20-04-1998');
+INSERT INTO professores (id, nome, email, senha, uf, data_nascimento) VALUES
+(199568921436, 'Ana Silva Souza', 'ana.silva@gmail.com', 'anass123', 'CE', '01-01-1995'),
+(199868921543, 'Roberto Gonçalves', 'roberto.goncalves@gmail.com', '12345678', 'CE', '20-04-1998'),
+(199260932821, 'Diego Morais', 'diego.morais@gmail.com', '12345678', 'CE', '07-08-1992');
 
-INSERT INTO coordenadores (id, nome, email, nascimento, senha) VALUES (1, 'Gabriel', 'gabriel@gmail.com', '04-05-1989', '12345678');
+INSERT INTO escolas_professores (escolas_id, professores_id) VALUES (68921, 199568921436), (68921, 199868921543), (60932, 199260932821);
 
-INSERT INTO gestores (id, nome, email, nascimento, senha) VALUES (1, 'Gabriela', 'gabriela@gmail.com', '04-05-1989', '12345678');
+INSERT INTO coordenadores (id, nome, email, nascimento, senha) VALUES (19896892132, 'Gabriel', 'gabriel@gmail.com', '04-05-1989', '12345678');
+INSERT INTO escolas_coordenadores (escolas_id, coordenadores_id) VALUES (68921, 19896892132);
 
-INSERT INTO alunos (id, nome, senha, data_nascimento, ano_matricula) VALUES (1, 'Matheus Soares do Nascimento', '12345678', '02-10-2004', '2010');
-INSERT INTO alunos (id, nome, senha, data_nascimento, ano_matricula) VALUES (2, 'Suyane Oliveira da Silva', 'abcdefgh', '08-10-2003', '2008');
-INSERT INTO alunos (id, nome, senha, data_nascimento, ano_matricula) VALUES (3, 'Taislene da Silva Gonçalves', 'abcdefgh', '03-10-2004', '2010');
-INSERT INTO alunos (id, nome, senha, data_nascimento, ano_matricula) VALUES (4, 'Rozane Raquel da Silva Gonçalves', 'abcdefgh', '10-06-2002', '2007');
+INSERT INTO gestores (id, nome, email, nascimento, senha) VALUES (198968921543, 'Gabriela', 'gabriela@gmail.com', '04-05-1989', '12345678');
+INSERT INTO escolas_gestores (escolas_id, gestores_id) VALUES (68921, 198968921543);
 
-INSERT INTO turmas (id, serie, letra) VALUES (1, 1, 'A');
-INSERT INTO turmas (id, serie, letra) VALUES (2, 1, 'B');
+INSERT INTO alunos (id, nome, senha, data_nascimento, ano_matricula) VALUES 
+(2010689216742, 'Matheus Soares do Nascimento', '12345678', '02-10-2004', '2010'),
+(2008689215321, 'Suyane Oliveira da Silva', 'abcdefgh', '08-10-2003', '2008'),
+(2010689215413, 'Taislene da Silva Gonçalves', 'abcdefgh', '03-10-2004', '2010'),
+(2007689216223, 'Rozane Raquel da Silva Gonçalves', 'abcdefgh', '10-06-2002', '2007');
 
-INSERT INTO materias (id, nome) VALUES (1, 'Português');
-INSERT INTO materias (id, nome) VALUES (2, 'Matematica');
-INSERT INTO materias (id, nome) VALUES (3, 'Ciencias');
-INSERT INTO materias (id, nome) VALUES (4, 'Historia');
-INSERT INTO materias (id, nome) VALUES (5, 'Geografia');
+INSERT INTO turmas (id, serie, letra) VALUES (689211426, 1, 'A'), (689211832, 1, 'B');
+INSERT INTO turmas_alunos (turmas_id, alunos_id) VALUES 
+(689211426, 2010689216742),
+(689211426, 2008689215321),
+(689211832, 2010689215413),
+(689211832, 2007689216223);
+INSERT INTO escolas_turmas (escolas_id, turmas_id) VALUES (68921, 689211426), (68921, 689211832);
+INSERT INTO escolas_alunos (escolas_id, alunos_id) VALUES
+(68921, 2010689216742),
+(68921, 2008689215321),
+(68921, 2010689215413),
+(68921, 2007689216223);
 
-INSERT INTO turmas_alunos (turmas_id, alunos_id) VALUES (1, 2);
-INSERT INTO turmas_alunos (turmas_id, alunos_id) VALUES (1, 1);
-INSERT INTO turmas_alunos (turmas_id, alunos_id) VALUES (2, 3);
-INSERT INTO turmas_alunos (turmas_id, alunos_id) VALUES (2, 4);
+INSERT INTO materias (id, nome) VALUES 
+(1, 'Português'),
+(2, 'Matematica'),
+(3, 'Ciencias'),
+(4, 'Historia'),
+(5, 'Geografia');
 
 INSERT INTO ufs (id, nome, sigla) VALUES
 (1, 'Acre', 'AC'),
@@ -56,17 +71,18 @@ INSERT INTO ufs (id, nome, sigla) VALUES
 (27, 'Tocantins', 'TO');
 
 INSERT INTO professores_turmas_materias (professores_id, turmas_id, materias_id) VALUES 
-(1, 1, 1),
-(1, 1, 2),
-(1, 1, 3),
-(1, 1, 4),
-(1, 1, 5),
-(2, 2, 1),
-(2, 2, 2),
-(2, 2, 3),
-(2, 2, 4),
-(2, 2, 5);
+(199568921436, 689211426, 1),
+(199568921436, 689211426, 2),
+(199568921436, 689211426, 3),
+(199568921436, 689211426, 4),
+(199568921436, 689211426, 5),
+(199868921543, 689211832, 1),
+(199868921543, 689211832, 2),
+(199868921543, 689211832, 3),
+(199868921543, 689211832, 4),
+(199868921543, 689211832, 5);
 
-INSERT INTO avaliacoes (id, serie, bimestre, data_aplicacao) VALUES
-(1, 5, 2, '20-06-2024'),
-(2, 6, 4, '20-06-2024')
+INSERT INTO avaliacoes (id, serie, bimestre, professor_id, turma_id, data_aplicacao) VALUES
+(12393742, 1, 2, 1, 1, '20-06-2024'),
+(14174829, 1, 4, 1, 1, '20-06-2024');
+INSERT INTO materias_avaliacoes (materias_id, avaliacoes_id) VALUES (3, 12393742), (1, 14174829)
