19cfc173d2fc7184a5abe961d7ebc9a2cd3cee20
Matheus Soares <matheus.soares10@aluno.ifce.edu.br>
2025-01-27 15:36:43 -0300

    Adição de get_student_notes() que lista as notas de um aluno, adição de get_student_average_general() que calcula a media na serie atual do aluno, adição de get_class_average_general() que calcula a media de uma turma

--- database/nota.py
+++ database/nota.py
@@ -1,6 +1,7 @@
 import sqlite3
 from database.banco import connect_db
-from database.connection_tables import materias_avaliacoes
+import database.avaliacao as avl
+import database.turma as turma
 from random import randint
 
 
@@ -82,3 +83,78 @@ def generate_nota_id(al_id, av_id):
         nota_id += str(randint(0, 9))
     
     return nota_id
+
+
+def get_student_notes(al_id):
+    """Devolve todas as notas de um aluno"""
+
+    connection, cursor = connect_db()
+
+    cursor.execute('SELECT * FROM notas WHERE aluno_id = ?', (str(al_id),))
+    rows = cursor.fetchall()
+    connection.close()
+
+    notas = {
+        '1 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '2 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '3 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '4 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '5 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '6 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '7 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '8 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        '9 ano': {'Português': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Matêmatica': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Ciencias': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Geografia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}, 'Historia': {'1 bim': None, '2 bim': None, '3 bim': None, '4 bim': None}},
+        }
+
+    for row in rows:
+        av = avl.get(row[2])
+        materia = avl.get_matter(av)
+
+        notas[f'{str(av.serie)} ano'][materia][f'{str(av.bimestre)} bim'] = row[3]
+
+    return notas
+
+
+def get_student_average_general(al_id, serie):
+    """Calcula a media de um aluno em uma determinada serie"""
+
+    notes = get_student_notes(al_id)
+    soma = 0
+    count = 0
+
+    for materia in notes[f'{str(serie)} ano'].values():
+        for nota in materia.values():
+            if nota != None:
+                soma += nota
+                count += 1
+    
+    media = soma/count
+    return media
+
+
+def get_class_average_general(alunos, tur_id):
+    """Calcula a media de uma classe"""
+
+    turma_ = turma.get(tur_id)
+    soma = 0
+    count = 0
+
+    for aluno in alunos:
+        media = get_student_average_general(aluno.al_id, turma_.serie)
+        soma += media
+        count += 1
+    
+    media = soma/count
+
+    return media
+
+
+def acess_notes_data(notes: dict, serie: int, bimestre: int, materia: str):
+    """Recebe um dicionario de notas, juntamente a serie, o bimestre e a materia e devolve a nota correspondente"""
+    if 0 <= serie >= 10:
+        return None
+    if 0 <= bimestre >= 5:
+        return None
+    
+    nota = notes[f'{str(serie)} ano'][materia][f'{str(bimestre)} bim']
+    return nota
